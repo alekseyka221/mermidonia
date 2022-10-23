c9714faa77789eb92c89a4d54d0f5f768fd22817
alex <alekseyka221@gmail.com>
2022-10-23 16:29:20 +0200

    initial commit

---
+++ Mermidonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Mermidonia.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
